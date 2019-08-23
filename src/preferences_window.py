7da98fd4d0af676d933edffeeb32d551016071cb
Rafael Mardojai CM <mardojai.cardenas@gmail.com>
2019-08-23 18:49:00 -0500

    Update preferences window

--- src/preferences_window.py
+++ src/preferences_window.py
@@ -15,12 +15,84 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from gi.repository import Gtk, Handy
+from gi.repository import GLib, Gtk, Handy
 
 @Gtk.Template(resource_path='/com/rafaelmardojai/Telex/preferences.ui')
 class TelexPreferencesWindow(Handy.PreferencesWindow):
     __gtype_name__ = 'TelexPreferencesWindow'
 
-    def __init__(self, **kwargs):
+    # Get config widgets
+    run_background = Gtk.Template.Child()
+    close_dialog = Gtk.Template.Child()
+    close_dialog_row = Gtk.Template.Child()
+
+    animations = Gtk.Template.Child()
+
+    def __init__(self, client, settings, app, **kwargs):
         super().__init__(**kwargs)
+
+        self.client = client
+        self.settings = settings
+        self.gtk_settings = Gtk.Settings.get_default()
+        self.app = app
+
+        # Load actual config
+        self.run_background.set_state(self.settings.get_value('run-background'))
+        self.close_dialog.set_state(self.settings.get_value('close-dialog'))
+
+        self.animations.set_state(self.settings.get_value('animations'))
+
+
+        run_background = self.settings.get_value('run-background')
+        show_dialog = self.settings.get_value('close-dialog')
+
+        if run_background:
+            self.close_dialog_row.props.visible = True
+
+    '''
+    BEHAVIOR FUNCTIONS
+    '''
+    @Gtk.Template.Callback()
+    def _on_run_background_switch_state(self, widget, state):
+        self.settings.set_value('run-background', GLib.Variant('b', state))
+        if state:
+            self.close_dialog_row.props.visible = True
+        else:
+            self.close_dialog_row.props.visible = False
+
+    @Gtk.Template.Callback()
+    def _on_close_dialog_switch_state(self, widget, state):
+        self.settings.set_value('close-dialog', GLib.Variant('b', state))
+
+    '''
+    SECURITY FUNCTIONS
+    '''
+    @Gtk.Template.Callback()
+    def _on_logout(self, widget):
+        dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.WARNING,
+            Gtk.ButtonsType.YES_NO, 'Do you want to terminate this session?')
+        response = dialog.run()
+        if response == Gtk.ResponseType.YES:
+            self.client.loop.create_task(self.__logout())
+
+        dialog.destroy()
+
+    async def __logout(self):
+        '''
+        Log out from TG
+        '''
+        try:
+            await self.client.log_out()
+        except Exception:
+            pass
+
+        self.app.quit()
+
+    '''
+    ADVANCED FUNCTIONS
+    '''
+    @Gtk.Template.Callback()
+    def _on_animations_switch_state(self, widget, state):
+        self.settings.set_value('animations', GLib.Variant('b', state))
+        self.gtk_settings.set_property('gtk-enable-animations', state)
 
