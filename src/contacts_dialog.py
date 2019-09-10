857f7d9f1e20b1440b68f28ae2b6c47a53e937e2
Rafael Mardojai CM <mardojai.cardenas@gmail.com>
2019-09-10 09:17:23 -0500

    Update contacts dialog

--- src/contacts_dialog.py
+++ src/contacts_dialog.py
@@ -15,8 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import asyncio
-
 from gi.repository import Gtk, Handy
 
 from telethon import functions, utils
@@ -30,53 +28,61 @@ class ContactsDialog(Handy.Dialog):
     stack = Gtk.Template.Child()
     contacts_list = Gtk.Template.Child()
 
-    def __init__(self, client, **kwargs):
+    def __init__(self, client, window, **kwargs):
         super().__init__(**kwargs)
 
         self.client = client
+        self.win = window
+
         self.client.loop.create_task(self.post_init())
 
-        self.popover = Gtk.PopoverMenu()
-        popoverbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        self.popover.add(popoverbox)
-
     async def post_init(self):
-        result = await self.client(functions.contacts.GetContactsRequest(
+        # Get contacts
+        self.result = await self.client(functions.contacts.GetContactsRequest(
             hash=0
         ))
 
-        if len(result.users) == 0:
+        # If results = 0 show contacts empty screen
+        if len(self.result.users) == 0:
             self.stack.set_visible_child_name('empty')
 
-        for user in result.users:
-            row = Gtk.ListBoxRow()
-            row.set_selectable(False)
-            box = Gtk.Box(margin=6, spacing=10, valign=Gtk.Align.CENTER)
-            row.add(box)
+        # If results add them to the contacts list
+        if len(self.result.users) > 0:
+            for user in self.result.users:
+                row = Gtk.ListBoxRow()
+                row.set_selectable(False)
+                box = Gtk.Box(margin=6, spacing=10, valign=Gtk.Align.CENTER)
+                row.add(box)
 
-            avatar = Avatar(utils.get_display_name(user))
-            box.pack_start(avatar, None, None, 0)
+                avatar = Avatar(utils.get_display_name(user))
+                box.pack_start(avatar, None, None, 0)
 
-            vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, halign=Gtk.Align.START)
-            box.pack_start(vbox, True, True, 0)
+                vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, halign=Gtk.Align.START)
+                box.pack_start(vbox, True, True, 0)
 
-            name = Gtk.Label(utils.get_display_name(user), halign=Gtk.Align.START)
-            vbox.pack_start(name, True, True, 0)
-            status = Gtk.Label('last seen', halign=Gtk.Align.START)
-            Gtk.StyleContext.add_class(status.get_style_context(), 'dim-label')
-            vbox.pack_start(status, True, True, 0)
+                name = Gtk.Label(utils.get_display_name(user), halign=Gtk.Align.START)
+                vbox.pack_start(name, True, True, 0)
+                status = Gtk.Label('last seen', halign=Gtk.Align.START)
+                Gtk.StyleContext.add_class(status.get_style_context(), 'dim-label')
+                vbox.pack_start(status, True, True, 0)
 
-            button = Gtk.MenuButton()
-            icon = Gtk.Image.new_from_icon_name('view-more-symbolic', Gtk.IconSize.BUTTON)
-            button.set_image(icon)
-            button.set_popover(self.popover)
-            button.props.valign=Gtk.Align.CENTER
-            box.pack_end(button, None, None, 0)
+                button = Gtk.MenuButton()
+                icon = Gtk.Image.new_from_icon_name('view-more-symbolic', Gtk.IconSize.BUTTON)
+                button.set_image(icon)
+                button.props.valign=Gtk.Align.CENTER
+                box.pack_end(button, None, None, 0)
 
-            self.contacts_list.add(row)
+                self.contacts_list.add(row)
 
-        if len(result.users) > 0:
+            self.contacts_list.connect_async('row-activated', self.load_chat)
             self.contacts_list.show_all()
+
             self.stack.set_visible_child_name('contacts')
 
+    async def load_chat(self, list, row):
+        # Load contact chat in main window:
+        self.client.loop.create_task(self.win.load_chat(self.result.users[row.get_index()]))
+
+        # Destroy contacts dialog:
+        self.destroy()
 
